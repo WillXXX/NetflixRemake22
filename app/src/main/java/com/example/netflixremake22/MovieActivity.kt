c8add64a769827ff5a8fc3eb29b744b10d0a4d7e
Williams.Silva <https://github.com/WillXXX/BeberAgua.git>
2022-07-25 16:55:57 -0300

    ClipToPadding Start

---
+++ app/src/main/java/com/example/netflixremake22/MovieActivity.kt
@@ -0,0 +1,15 @@
+package com.example.netflixremake22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class MovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+
+        val toolbar: Toolbar = findViewById(R.id.movie_toolbar)
+        setSupportActionBar(toolbar)
+    }
+}
