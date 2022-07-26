a88535dff285d461742e8130e458e5337c70a30a
Williams.Silva <https://github.com/WillXXX/BeberAgua.git>
2022-07-26 16:49:38 -0300

    layout activity_movie

--- app/src/main/java/com/example/netflixremake22/MovieActivity.kt
+++ app/src/main/java/com/example/netflixremake22/MovieActivity.kt
@@ -1,8 +1,11 @@
 package com.example.netflixremake22
 
+import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,18 @@ class MovieActivity : AppCompatActivity() {
 
         val toolbar: Toolbar = findViewById(R.id.movie_toolbar)
         setSupportActionBar(toolbar)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //buscar desenhavel(layerlist)
+        val layerDrawable: LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+        //buscar filme()
+        val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4)
+        //atribui a essse layerList novo filme
+        layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
+        //setar no imageView
+        val coverImg: ImageView = findViewById(R.id.movie_img)
+        coverImg.setImageDrawable(layerDrawable)
     }
 }
